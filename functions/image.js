f7b0418a273b34be82f72ce7296be5ba7703bc3e
Jacob Godbout <jacob.godbout@deswik.com>
2024-07-18 16:42:08 +1000

    use new appraoch

---
+++ functions/image.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+
+exports.handler = async (event, context) => {
+  const startDate = new Date('2024-07-18');
+  const currentDate = new Date();
+  const timeDifference = currentDate - startDate;
+  const daysSinceStart = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const imageName = `${daysSinceStart}.jpg`;
+
+  const imagePath = path.join(__dirname, '../images', imageName);
+  
+  try {
+    const imageBuffer = fs.readFileSync(imagePath);
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: imageBuffer.toString('base64'),
+      isBase64Encoded: true,
+    };
+  } catch (error) {
+    return {
+      statusCode: 404,
+      body: 'Image not found',
+    };
+  }
+};
