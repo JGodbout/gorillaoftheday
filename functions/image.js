8888aa643a0f451fa3c80011840e77e4e3ee1b2f
Jacob Godbout <jacob.godbout@deswik.com>
2024-07-18 17:07:22 +1000

    use new appraoch

--- functions/image.js
+++ functions/image.js
@@ -1,30 +1,16 @@
-const path = require('path');
-const fs = require('fs');
-
 exports.handler = async (event, context) => {
-  const startDate = new Date('2024-07-18');
-  const currentDate = new Date();
-  const timeDifference = currentDate - startDate;
-  const daysSinceStart = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  const imageName = `${daysSinceStart}.jpg`;
-
-  const imagePath = path.join(process.cwd(), 'images', imageName);
-
-  try {
-    const imageBuffer = fs.readFileSync(imagePath);
+    const startDate = new Date('2024-07-18');
+    const currentDate = new Date();
+    const timeDifference = currentDate - startDate;
+    const daysSinceStart = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const imageName = `${daysSinceStart}.jpg`;
     
+    const imageUrl = `https://${process.env.URL}/images/${imageName}`;
+  
     return {
-      statusCode: 200,
+      statusCode: 302,
       headers: {
-        'Content-Type': 'image/jpeg',
+        Location: imageUrl,
       },
-      body: imageBuffer.toString('base64'),
-      isBase64Encoded: true,
     };
-  } catch (error) {
-    return {
-      statusCode: 404,
-      body: `Image not found: ${imagePath}`,
-    };
-  }
-};
+  };
